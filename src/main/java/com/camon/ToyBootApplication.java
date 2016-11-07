6fc3d16940f8b259b0594224c8202f790b13216f
camon <camon85@gmail.com>
2016-11-08 01:15:01 +0900

    init

---
+++ src/main/java/com/camon/ToyBootApplication.java
@@ -0,0 +1,12 @@
+package com.camon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToyBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToyBootApplication.class, args);
+	}
+}
