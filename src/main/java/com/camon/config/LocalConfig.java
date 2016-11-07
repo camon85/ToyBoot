67f572691ea364d9eec5cbca5976a3ff08e96fd0
camon <camon85@gmail.com>
2016-11-08 02:33:02 +0900

    redis

--- src/main/java/com/camon/config/LocalConfig.java
+++ src/main/java/com/camon/config/LocalConfig.java
@@ -1,8 +1,12 @@
 package com.camon.config;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 @Profile({"default", "local"})
@@ -13,7 +17,21 @@ public class LocalConfig extends CommonConfig {
         log.info("### LocalConfig");
     }
 
-
+    @Bean
+    public JedisConnectionFactory connectionFactory() {
+        JedisConnectionFactory connectionFactory = new JedisConnectionFactory();
+        connectionFactory.setHostName("52.78.200.185");
+        connectionFactory.setPort(5000);
+        connectionFactory.setPassword("foobared");
+        return connectionFactory;
+    }
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate() {
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<String, Object>();
+        redisTemplate.setConnectionFactory(connectionFactory());
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        return redisTemplate;
+    }
 
 
 }
--- src/main/java/com/camon/controller/HelloController.java
+++ src/main/java/com/camon/controller/HelloController.java
@@ -1,17 +1,28 @@
 package com.camon.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Set;
 
 /**
  * Created by camon on 2016-11-03.
  */
 @Controller
 public class HelloController {
+
+    @Autowired
+    private RedisTemplate<String, Object> redisTemplate;
+
+
     @RequestMapping("/")
     @ResponseBody
     String home() {
+        Set<String> foo = redisTemplate.keys("*");
         return "Hello World!";
     }
 }
